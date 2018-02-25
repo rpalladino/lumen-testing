04db1af871606db4d310da8195b653d8a1555190
Rocco Palladino <rpalladino@gmail.com>
2018-02-25 08:53:54 -0600

    Greet a person by full name, looking them up by last name, with unit test

--- app/Http/Controllers/ExampleController.php
+++ app/Http/Controllers/ExampleController.php
@@ -2,10 +2,33 @@
 
 namespace App\Http\Controllers;
 
+use Example\Person\PersonRepository;
+
 class ExampleController extends Controller
 {
+    /**
+     * @var PersonRepository
+     */
+    private $personRepository;
+
+    public function __construct(PersonRepository $personRepository)
+    {
+        $this->personRepository = $personRepository;
+    }
+
     public function hello() : string
     {
         return "Hello World!";
+    }
+
+    public function helloPerson(string $lastName) : string
+    {
+        $foundPerson = $this->personRepository->findByLastName($lastName);
+
+        return $foundPerson
+            ->map(function ($person) {
+                return "Hello {$person->firstName} {$person->lastName}!";
+            })
+            ->get();
     }
 }
--- tests/ExampleControllerTest.php
+++ tests/ExampleControllerTest.php
@@ -1,15 +1,20 @@
 <?php
 
 use App\Http\Controllers\ExampleController;
+use Example\Person\Person;
+use Example\Person\PersonRepository;
+use PhpOption\Some;
 use PHPUnit\Framework\TestCase;
 
 class ExampleControllerTest extends TestCase
 {
     private $subject;
+    private $personRepository;
 
     public function setUp()
     {
-        $this->subject = new ExampleController();
+        $this->personRepository = Mockery::mock(PersonRepository::class);
+        $this->subject = new ExampleController($this->personRepository);
     }
 
     /**
@@ -18,5 +23,23 @@ class ExampleControllerTest extends TestCase
     public function shouldReturnHelloWorld()
     {
         assertThat($this->subject->hello(), is("Hello World!"));
+    }
+
+    /**
+     * @test
+     */
+    public function shouldReturnFullNameOfAPerson()
+    {
+        $peter = new Person();
+        $peter->firstName = "Peter";
+        $peter->lastName = "Pan";
+
+        $this->personRepository->allows()
+             ->findByLastName("Pan")
+             ->andReturns(new Some($peter));
+
+        $greeting = $this->subject->helloPerson("Pan");
+
+        assertThat($greeting, is("Hello Peter Pan!"));
     }
 }
