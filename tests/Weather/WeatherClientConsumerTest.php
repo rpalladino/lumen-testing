9b212c0de16cd0a77fb7d8a9cbfbd40a305f1255
Rocco Palladino <rpalladino@gmail.com>
2018-03-05 19:51:31 -0600

    Separate creation of the pact and testing the behavior in the weather consumer contract test

--- tests/Weather/WeatherClientConsumerTest.php
+++ tests/Weather/WeatherClientConsumerTest.php
@@ -17,40 +17,51 @@ class WeatherClientConsumerTest extends TestCase
     const WEATHER_URL = '/forecast/some-api-key/53.5511,9.9937';
 
     /**
+     * @var MockServerEnvConfig
+     */
+    private $config;
+
+    /**
+     * @var InteractionBuilder
+     */
+    private $pact;
+
+    /**
+     * Create the pact between consumer and provider
+     * 
+     * @before
+     */
+    public function createPact()
+    {
+        $this->config  = new MockServerEnvConfig();
+        $this->pact = new InteractionBuilder($this->config);
+        $this->pact
+            ->given("Weather forecast data")
+            ->uponReceiving("A request for a forecast")
+            ->with((new ConsumerRequest())
+                ->setMethod('GET')
+                ->setPath(self::WEATHER_URL)
+            )
+            ->willRespondWith((new ProviderResponse())
+                ->setStatus(200)
+                ->setBody(FileLoader::read(__DIR__.'/weatherApiResponse.json'))
+            );
+    }
+
+    /**
      * @test
      */
     public function shouldFetchCurrentWeatherInformation()
     {
-        // Create request expected from the weather client
-        $request = new ConsumerRequest();
-        $request
-            ->setMethod('GET')
-            ->setPath(self::WEATHER_URL);
-
-        // Create response expected from the weather service
-        $response = new ProviderResponse();
-        $response
-            ->setStatus(200)
-            ->setBody(FileLoader::read(__DIR__.'/weatherApiResponse.json'));
-
-        // Create the pact between consumer and provider
-        $config  = new MockServerEnvConfig();
-        $builder = new InteractionBuilder($config);
-        $builder
-            ->given("Weather forecast data")
-            ->uponReceiving("A request for a forecast")
-            ->with($request)
-            ->willRespondWith($response);
-
         // Create the weather client consumer
-        $httpClient = new Client(['base_uri' => $config->getBaseUri()]);
+        $httpClient = new Client(['base_uri' => $this->config->getBaseUri()]);
         $weatherClient = new WeatherClient($httpClient, self::WEATHER_URL);
 
         // Make request against the mock weather service
         $weatherResponse = $weatherClient->currentWeather();
 
         // Verify that interactions defined in the pact took place
-        $builder->verify();
+        $this->pact->verify();
 
         assertThat($weatherResponse->isDefined(), is(true));
         assertThat($weatherResponse->get()->getSummary(), is("Rain"));
